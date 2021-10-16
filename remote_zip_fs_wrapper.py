bba63ec6047af37f6367cf75b9fc862e7063e7f5
Omar Wasfy <eng.omarwasfy@gmail.com>
2021-10-16 12:48:34 +0200

    Update remote_zip_fs_wrapper.py

--- remote_zip_fs_wrapper.py
+++ remote_zip_fs_wrapper.py
@@ -1,35 +1,41 @@
+!pip install remotezip
 from remotezip import RemoteZip
 class remote_zip_fs_wrapper:
-  def __init__(self, zip_url,debug=0):
-    set_zip(zip_url,debug)
-  def zip_info_list(zip_url):
-    with RemoteZip(zip_url) as zip:
-      infolist=zip.infolist()
-      filelist=zip.filelist()
-      zip.close()
-      return infolist,filelist
-  def zip_extract_file(file_path):
-    self.zip.extract(file_path)
-  def zip_extract_file(zip_url,file_path):
-    with RemoteZip(zip_url) as zip:
-      zip.extract(file_path)
-      zip.close()
-  def zip_open_file(file_path):
-     with self.zip.open(file_path) as zip_open_file:
-        return zip_open_file.read()
-  def zip_open_file(zip_url,file_path):
-    with RemoteZip(zip_url) as zip:
-      with self.zip.open(file_path) as zip_open_file:
-        fileread=zip_open_file.read()
-        zip_open_file.close()
-        zip.close()
-        return fileread()
-  def set_zip(zip_url):
+  zip_url=None
+  zip=None
+  infolist=[]
+  filelist=[]
+  def set_zip(self,zip_url,debug):
     self.zip_url=zip_url
     with RemoteZip(zip_url) as zip:
       self.zip =zip
       self.zip.debug=debug
       self.infolist=zip.infolist()
-      self.filelist=zip.filelist()
-  def get_zip():
+      self.filelist=[]
+      for zip_info in zip.infolist():
+        self.filelist.append(zip_info.filename)
+  def __init__(self, zip_url,debug):
+    self.set_zip(zip_url,debug)
+  def tmp_zip_info_list(self,zip_url):
+    with RemoteZip(zip_url) as zip:
+      infolist=zip.infolist()
+      filelist=[]
+      for zip_info in zip.infolist():
+        filelist.append(zip_info.filename)
+      return infolist,filelist
+  def tmp_zip_extract_file(self,zip_url,file_path):
+    with RemoteZip(zip_url) as zip:
+      zip.extract(file_path)
+  def zip_extract_file(self,file_path):
+    self.tmp_zip_extract_file(self.zip_url,file_path)
+  def tmp_zip_open_file(self,zip_url,file_path):
+    with RemoteZip(zip_url) as zip:
+      with zip.open(file_path) as zip_open_file:
+        fileread=zip_open_file.read()
+        return fileread
+  def zip_open_file(self,file_path):
+     self.tmp_zip_open_file(self.zip_url,file_path)
+  def get_zip(self):
     return self.zip_url,self.zip,self.infolist,self.filelist
+  def close_zip(self):
+    self.zip.close()
